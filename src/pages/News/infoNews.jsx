c3c03fe7b78d8c2a93c8640629aeaedfca70c145
Langony ST <langonyst@gmail.com>
2023-07-05 11:33:11 -0300

    feat: adicionado navegação e função ´para deletar noticia

--- src/pages/News/infoNews.jsx
+++ src/pages/News/infoNews.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const InfoNews = () => {
 
     const { id } = useParams();
 
     const [news, setNews] = useState([]);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
 
@@ -21,9 +24,29 @@ const InfoNews = () => {
             })
     }, [])
 
+    const handleClick = () => {
+        navigate(`/update-news/${id}`)
+    }
+
+    const handleDelete = async () => {
+        try{
+            const response = await api.delete(`/news/${id}`);
+            console.log('News deleted');
+            navigate(-1);
+        }catch(error){
+            console.error('Error ', error);
+        }
+    }
+
     return (
         <div>
             <h1>{news.title}</h1>
+            <p>
+                <button onClick={handleClick}>Alterar registro</button>
+            </p>
+            <p>
+                <button onClick={handleDelete}>Deletar registro</button>
+            </p>
             <p>Content: {news.body}</p>
             <p>Author: {news.author}</p>
         </div>

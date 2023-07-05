54083975cd3661e8213a338ebedef5a0acf51e38
Langony ST <langonyst@gmail.com>
2023-07-05 12:58:54 -0300

    feat: adicionado funções no index de users

--- src/pages/Users/index.jsx
+++ src/pages/Users/index.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import api from '../../services/api.js'
+import { Link, useNavigate } from 'react-router-dom';
 
 const Users = () => {
     const [users, setUser] = useState([]);
 
+    const navigate = useNavigate();
+    
     useEffect(() => {
         api.get('/users')
             .then((response) => {
@@ -15,16 +18,21 @@ const Users = () => {
             })
     }, [])
 
+    const handleClick = () => {
+        navigate('/add-user')
+    }
+
     return (
         <div className="posts-container">
+            <h1>Usuários:</h1>
+            <p>
+                <button onClick={handleClick}>Adicionar usuário</button>
+            </p>
             {users.map((user) => {
                 return (
                     <div className="post-card" key={user._id}>
-                        <h2 className="post-title">{user.name}</h2>
+                        <h2 className="post-title"><Link to={`/info-user/${user._id}`}>{user.name}</Link></h2>
                         <p className="post-body">{user.email}</p>
-                            <div className="button">
-                                <div className="delete-btn">Delete</div>
-                            </div>
                     </div>
                 );
             })}

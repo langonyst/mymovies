41aa5d7bf11c5ba293b5143f8c1c14dacf549d0a
Langony ST <langonyst@gmail.com>
2023-07-05 11:16:57 -0300

    feat: adicionado tela para cadastrar noticia

--- src/pages/News/addNews.jsx
+++ src/pages/News/addNews.jsx
@@ -1,11 +1,71 @@
 import React, { useState, useEffect } from 'react';
 import api from '../../services/api.js';
+import { useNavigate } from 'react-router-dom';
 
 const AddNews = () => {
+    const [formData, setFormData] = useState({
+        title: '',
+        body: '',
+        author: ''
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (event) => {
+        setFormData({
+          ...formData,
+          [event.target.name]: event.target.value
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        
+        try {
+          const response = api.post('/news', formData);
+          console.log('News created:', response.data);
+          navigate(-1)
+        } catch (error) {
+          console.error('Error creating news:', error);
+        }
+    };
 
     return (
         <div>
             <h1>Adicionar noticia</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Titulo:
+                    <input
+                    type="text"
+                    name="title"
+                    value={formData.title}
+                    onChange={handleChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Noticia:
+                    <input
+                    type="text"
+                    name="body"
+                    value={formData.body}
+                    onChange={handleChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Autor:
+                    <input
+                    type="text"
+                    name="author"
+                    value={formData.author}
+                    onChange={handleChange}
+                    />
+                </label>
+                <br />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }

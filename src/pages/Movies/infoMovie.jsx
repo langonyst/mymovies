f6a5b661673767af0f97763d9f45b3dbe6e0fdca
Langony ST <langonyst@gmail.com>
2023-07-05 10:26:23 -0300

    feat: adicionado navegação nas informações do filme, botão de alteração de registro e deletar registro

--- src/pages/Movies/infoMovie.jsx
+++ src/pages/Movies/infoMovie.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const InfoMovie = () => {
 
     const { id } = useParams();
-
+    const navigate = useNavigate();
     const [movie, setMovie] = useState([]);
 
     useEffect(() => {
@@ -21,11 +21,32 @@ const InfoMovie = () => {
             })
     }, [])
 
+    const handleClick = () => {
+        navigate(`/update-movie/${id}`)
+    }
+
+    const handleDelete = async () => {
+        try{
+            const response = await api.delete(`/movies/${id}`);
+            console.log('Sucess!');
+            console.log(response.data)
+            navigate(-1);
+        }catch(error){
+            console.error('Error delete movie ', error)
+        }
+    }
+
     return (
         <div>
             <h1>Info Movie: {movie.title}</h1>
+            <p>
+                <button onClick={handleClick}>Alterar registro</button>
+            </p>
+            <p>
+                <button onClick={handleDelete}>Deletar registro</button>
+            </p>
             <p>Full plot: {movie.fullplot}</p>
-            <p>Released: {movie.released}</p>
+            <p>Released: {movie.year}</p>
             <p>Director: {movie.directors}</p>
         </div>
     )
